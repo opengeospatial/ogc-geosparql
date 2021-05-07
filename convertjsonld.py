975e64fef5fa342e923c524901a33cae0161c298
Timo <situx@users.noreply.github.com>
2021-05-07 18:21:59 +0200

    Update convertjsonld.py

--- convertjsonld.py
+++ convertjsonld.py
@@ -4,9 +4,5 @@ import sys
 
 g = Graph().parse(sys.argv[1], format='turtle')
 
-context = {"@vocab": "http://purl.org/dc/terms/", "@language": "en"}
-jsonld=g.serialize(format='json-ld', context=context, indent=2)
- 
-f = open(sys.argv[2], "w")
-f.write(jsonld)
-f.close()
+#context = {"@vocab": "http://purl.org/dc/terms/", "@language": "en"}
+jsonld=g.serialize(format='json-ld', indent=2,destination=sys.argv[2])
