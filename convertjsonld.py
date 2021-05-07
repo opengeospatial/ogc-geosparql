01d1ed662543e0c490aeb920dbbd13bd289d2bdb
Timo <situx@users.noreply.github.com>
2021-05-07 18:13:32 +0200

    Update convertjsonld.py

--- convertjsonld.py
+++ convertjsonld.py
@@ -2,11 +2,11 @@ from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
 import sys
 
-g = Graph().parse(data=sys.argv[0], format='turtle')
+g = Graph().parse(data=sys.argv[1], format='turtle')
 
 context = {"@vocab": "http://purl.org/dc/terms/", "@language": "en"}
 jsonld=g.serialize(format='json-ld', context=context, indent=4)
  
-f = open(sys.argv[1], "w")
+f = open(sys.argv[2], "w")
 f.write(jsonld)
 f.close()
