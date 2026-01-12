52316ddb286c819aa84aa38f864e9f4823860297
Timo Homburg <situx@users.noreply.github.com>
2026-01-12 14:19:42 +0100

    union instead of add

--- scripts/processbibtex.py
+++ scripts/processbibtex.py
@@ -12,7 +12,7 @@ for file in os.listdir(directory):
             content=f.read()
             matches=re.findall("<<([A-Za-z]+)>>",content)
             #print("MATCHES: "+str(matches))
-            citedlabels.add(set(matches))
+            citedlabels.union(matches)
         continue
     else:
         continue
