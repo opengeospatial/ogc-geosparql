e49e71dc81c216924160d8088a4468b4fa8a5ed0
situx <situx@users.noreply.github.com>
2026-03-05 20:40:30 +0000

    Update directories from 13buildingblocks

---
+++ bblocks/config.js
@@ -0,0 +1,6 @@
+window.bblocksRegister = 'https://opengeospatial.github.io/ogc-geosparql/build/register.json';
+window.bblocksViewer = {
+  title: "GeoSPARQL",
+  showImported: 0,
+  baseUrl: '/ogc-geosparql/bblocks/',
+};
