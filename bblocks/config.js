c63761165daf86014b85368ee90876c4ce0bdfdd
jabhay <joseph.abhayaratna@gmail.com>
2026-03-18 20:14:47 +0000

    Deploying to gh-pages from @ opengeospatial/ogc-geosparql@7af3a5f3d70096bf72610e5adee51026b04ec78c 🚀

--- bblocks/config.js
+++
@@ -1,6 +0,0 @@
-window.bblocksRegister = 'https://opengeospatial.github.io/ogc-geosparql/build/register.json';
-window.bblocksViewer = {
-  title: "GeoSPARQL",
-  showImported: 0,
-  baseUrl: '/ogc-geosparql/bblocks/',
-};
