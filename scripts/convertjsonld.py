84bd437f52be2624e2b56d2178ebb886d32a83a2
Nicholas Car <nick@kurrawong.ai>
2026-08-06 13:12:33 +1000

    restore scripts

---
+++ scripts/convertjsonld.py
@@ -0,0 +1,8 @@
+from rdflib import Graph, plugin
+from rdflib.serializer import Serializer
+import sys
+
+g = Graph().parse(sys.argv[1], format='turtle')
+
+g.serialize(format='json-ld', indent=2, destination=sys.argv[2])
+g.serialize(format='n3', destination=sys.argv[3])
